5ec74c2e4cc748f320737fddb7071dfd405f5181
Louis Radtke <louis.radtke@tu-dortmund.de>
2026-01-29 00:49:18 +0100

    continue cleanup

--- collect_git.sh
+++ collect_git.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 # shellcheck disable=SC2028
+
 echo "\\newcommand{\\gitcommit}[0]{$(git rev-parse --short HEAD)}" > variables.tex
 
-# shellcheck disable=SC2028
-echo "\\newcommand{\\giturl}[0]{https://github.com/louisradtke/cv/tree/$(git rev-parse --short HEAD)}" >> variables.tex
+if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
+    git_ref="$(git rev-parse --short HEAD)"
+else
+    git_ref="main"
+fi
+echo "\\newcommand{\\giturl}[0]{https://github.com/louisradtke/cv/tree/${git_ref}}" >> variables.tex
